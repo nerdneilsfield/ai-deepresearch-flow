473598900902da12b34c8fc9c0535d1e6f2aa470
dengqi <dengqi935@gmail.com>
2026-01-19 13:28:13 +0800

    feat(phi): add philosophy prompt templates

--- python/deepresearch_flow/paper/template_registry.py
+++ python/deepresearch_flow/paper/template_registry.py
@@ -34,6 +34,13 @@ _TEMPLATES: dict[str, TemplateBundle] = {
         schema_file="default_paper_schema.json",
         render_template="default_paper.md.j2",
     ),
+    "simple_phi": TemplateBundle(
+        name="simple_phi",
+        prompt_system="simple_phi_system.j2",
+        prompt_user="simple_phi_user.j2",
+        schema_file="default_paper_schema.json",
+        render_template="default_paper.md.j2",
+    ),
     "deep_read": TemplateBundle(
         name="deep_read",
         prompt_system="deep_read_system.j2",
@@ -41,10 +48,24 @@ _TEMPLATES: dict[str, TemplateBundle] = {
         schema_file="deep_read_schema.json",
         render_template="deep_read.md.j2",
     ),
+    "deep_read_phi": TemplateBundle(
+        name="deep_read_phi",
+        prompt_system="deep_read_phi_system.j2",
+        prompt_user="deep_read_phi_user.j2",
+        schema_file="deep_read_phi_schema.json",
+        render_template="deep_read_phi.md.j2",
+    ),
     "eight_questions": TemplateBundle(
         name="eight_questions",
         prompt_system="eight_questions_system.j2",
         prompt_user="eight_questions_user.j2",
+        schema_file="eight_questions_schema.json",
+        render_template="eight_questions.md.j2",
+    ),
+    "eight_questions_phi": TemplateBundle(
+        name="eight_questions_phi",
+        prompt_system="eight_questions_phi_system.j2",
+        prompt_user="eight_questions_phi_user.j2",
         schema_file="eight_questions_schema.json",
         render_template="eight_questions.md.j2",
     ),
@@ -75,7 +96,25 @@ _STAGES: dict[str, list[StageDefinition]] = {
         StageDefinition("module_g", ["module_g"]),
         StageDefinition("module_h", ["module_h"]),
     ],
+    "deep_read_phi": [
+        StageDefinition("module_m1", ["module_m1"]),
+        StageDefinition("module_m2", ["module_m2"]),
+        StageDefinition("module_m3", ["module_m3"]),
+        StageDefinition("module_m4", ["module_m4"]),
+        StageDefinition("module_m5", ["module_m5"]),
+        StageDefinition("module_m6", ["module_m6"]),
+    ],
     "eight_questions": [
+        StageDefinition(
+            "questions_1to4",
+            ["question1", "question2", "question3", "question4"],
+        ),
+        StageDefinition(
+            "questions_5to8",
+            ["question5", "question6", "question7", "question8"],
+        ),
+    ],
+    "eight_questions_phi": [
         StageDefinition(
             "questions_1to4",
             ["question1", "question2", "question3", "question4"],
